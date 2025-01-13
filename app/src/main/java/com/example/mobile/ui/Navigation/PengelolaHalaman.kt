ded68e2c278512c0aa93781188126f7835d3882b
cahyadie <cahyadiilham12@gmail.com>
2025-01-13 08:49:08 +0700

    menambahkan PengelolaHalaman

---
+++ app/src/main/java/com/example/mobile/ui/Navigation/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.mobile.ui.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mobile.ui.pages.HomeScreen
+import com.example.mobile.ui.pages.InsertMhsView
+
+@Composable
+fun PengelolaHalaman (
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = {navController.popBackStack()},
+                onNavigate = {
+                    navController.navigate(DestinasiInsert.route)
+                }
+            )
+        }
+    }
+}
